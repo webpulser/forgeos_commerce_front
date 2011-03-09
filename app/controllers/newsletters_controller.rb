d70a0fb92ebb36a3dcd10ca8f26680b6a7e47218
Sébastien Dumont <sebastien.dumont@webpulser.com>
2011-03-09 10:47:26 +0100

    fix newsletter notification

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -2,9 +2,9 @@ class NewslettersController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    newsletter = Newsletter.new(params[:newsletter])
+    newsletter = Newsletter.new(params[:newsletter].to_s.strip)
     if newsletter.save
-      flash[:notice] = "Vous êtes maintenant inscrit à la newsletter de confort du fil"
+      flash[:notice] = t(:success, :scope => [:create, :newsletter])
       @page = Page.find_by_single_key('inscription_reussie')
       return redirect_to :back
     else
