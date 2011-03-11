4aeedaad46004c80fefe8805991acd04c0e88079
Sébastien Dumont <sebastien.dumont@webpulser.com>
2011-03-11 09:47:41 +0100

    update newsletter

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -2,15 +2,15 @@ class NewslettersController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    newsletter = Newsletter.new(params[:newsletter].to_s.strip)
+    newsletter = Newsletter.new(params[:newsletter])
+    newsletter.email = newsletter.email.to_s.strip
     if newsletter.save
       flash[:notice] = t(:success, :scope => [:create, :newsletter])
       @page = Page.find_by_single_key('inscription_reussie')
-      return redirect_to :back
     else
       flash[:error] = newsletter.errors.full_messages.first
-      return redirect_to :back
     end
+    return redirect_to :back
   end
 
 end
