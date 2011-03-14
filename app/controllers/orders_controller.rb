dd9064692a625ca3f4bc5d94772a086326318649
Sébastien Dumont <sebastien.dumont@webpulser.com>
2011-03-14 09:31:28 +0100

    update account

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,17 @@
 class OrdersController < ApplicationController
 
   def index
-    @orders = Order.paginate(:conditions => {:status => ['paid', 'shipped']}, :order => :updated_at)
+    @orders = Order.paginate(:conditions => {:status => ['paid', 'shipped']}, :order => :updated_at, :page => page, :per_page => per_page)
+  end
+
+private
+
+  def page
+    params[:page].to_i > 0 ? params[:page] : 1
+  end
+
+  def per_page
+    params[:per_page].to_i > 0 ? params[:per_page] : 10
   end
 
 end
