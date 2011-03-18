5b8b34e940aa74d7fc55033670b4d4b596de6e84
Jean-Charles Lefrancois <jcl@webpulser.com>
2011-03-18 17:44:15 +0100

    add notifier for order_confirmation

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -34,6 +34,39 @@ class Notifier < ActionMailer::Base
     content_type "text/html"
     body[:email] = email
   end
+  
+  def order_confirmation(user, order)
+    content_type "multipart/alternative"
+    recipients user.email
+    from Setting.first.email
+    subject "[#{application}] #{I18n.t(:subject, :scope => [:emails, :order_confirmation])}"
+    sent_on Time.now
+    
+    part :content_type => 'text/html', :body => render_message(
+      'order_confirmation',
+      :user => user,
+      :order => order,
+      :address_invoice => order.address_invoice,
+      :address_delivery => order.address_delivery,
+      :url => url_for(:action=>"root", :controller=>"url_catcher")
+      )
+      
+#    current_body = {
+#      :user_fullname => order.user.fullname,
+#      :order_total => order.total,
+#      :order => order,
+#      :order_details => order.order_details
+#    }
+
+#   attachment "application/pdf" do |a|
+#      a.filename = "facture_rue_du_store_#{order.reference}.pdf"
+#      html = render(:file => '/orders/show.html.haml', :body => current_body, :layout => 'order_pdf')
+
+#      kit = PDFKit.new(html, :title => "Commande #{order.reference}" )
+#      kit.stylesheets = ["#{RAILS_ROOT}/public/stylesheets/front/invoice-print.css" ]
+#      a.body = kit.to_pdf
+#    end
+  end
 
   private
   def price_with_currency(price)
