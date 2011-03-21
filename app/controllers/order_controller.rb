74b13b781545e06319b88406647f2c23450cfdff
Jean-Charles Lefrancois <jcl@webpulser.com>
2011-03-21 11:53:47 +0100

    working on payment method

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,7 +1,8 @@
+require 'sha1'
 class OrderController < ApplicationController
   before_filter :must_be_logged, :only => [:new, :deliveries]
   before_filter :validate_and_update_address, :only => [:new]
-  skip_before_filter :verify_authenticity_token, :only => [:call_autoresponse, :paypal_notification]
+  skip_before_filter :verify_authenticity_token, :only => [:call_autoresponse, :paypal_notification, :succes, :cancel]
 
   def new
     special_offer
@@ -40,37 +41,112 @@ class OrderController < ApplicationController
             @sChaineDebug = "V1.04.sha1.rb--[CtlHmac" + @oTpe.sVersion + @oTpe.sNumero + "]-" + @oMac.computeHMACSHA1("CtlHmac" + @oTpe.sVersion + @oTpe.sNumero) # Control String for support
             @sChaineMAC = [@oTpe.sNumero, @sDate, "#{@sMontant}#{@sDevise}", @sReference, @sTexteLibre, @oTpe.sVersion, @sLangue, @oTpe.sCodeSociete, @sEmail, @sNbrEch, @sDateEcheance1, @sMontantEcheance1, @sDateEcheance2, @sMontantEcheance2, @sDateEcheance3, @sMontantEcheance3, @sDateEcheance4, @sMontantEcheance4, @sOptions].join("*") # Data to certify         
           when "cyberplus"
-            @url = '/'
-            @site_id = "deded"
-            @mode = "TEST"
-            @page_action = "PAYMENT"
-            @action_mode = "INTERACTIVE"
-            @payment_config = "SINGLE"
-            @vads_version 	= "V2"
-            @sReference = "#{rand(1000)}A#{@order.reference}"
-            @sDevise  = "978"
-            @sMontant = '%.2f' % @order.total*100
-            @key = "sdsdfgsdfgsdfg"
-            values = [
-              @site_id,
-              @mode,
-              @payment,
-              @action_mode,
-              @payment_config,
-              @vads_version,
-              @sReference,
-              @sDevise,
-              @sMontant,
-            ]
-            values = values.sort.join('+')
-            @sChaineMAC = values + @key
+            ts = Time.now
+
+            @payment = {
+              :version => 'V1',
+              :site_id => '49234738',
+              :ctx_mode => 'TEST',
+              :trans_id => ts.strftime('%H%M%S'),
+              :trans_date => ts.strftime('%Y%m%d%H%M%S'),
+              :validation_mode => '',
+              :capture_delay => '',
+              :payment_config => 'SINGLE',
+              :payment_cards => 'VISA;MASTERCARD;CB',
+              :amount => (@order.total*100).to_i,
+              :currency => '978',
+              :key => '6852324211274299',
+              :url_cancel => url_for(:action => 'cancel'),
+              :url_success => url_for(:action => 'success'),
+              :url_refused => url_for(:action => 'cancel'),
+              :url_return => cart_path,
+              :url_referral => url_for(:action => 'new'),
+              :order_id => @order.id
+            }
+
+            sign = [:version, :site_id, :ctx_mode, :trans_id,
+                     :trans_date, :validation_mode, :capture_delay,
+                     :payment_config, :payment_cards, :amount,
+                     :currency, :key].map{ |key| @payment[key] }
+                     
+            @payment[:signature] = SHA1.new(sign.join('+'))
+
         end
       end
     else
-      #TODO use trnaslations
+      #TODO use translations
       flash[:error] = "Vous devez accepter les conditions générales de vente"
       render :action => 'new'
     end
+  end
+  
+  
+  def call_autoresponse
+#    TODO check payment autoresponse
+#    oTpe = CMCIC_Tpe.new()
+#    oHmac = CMCIC_Hmac.new(oTpe)
+
+#    sChaineMAC = [oTpe.sNumero, params["date"], params['montant'], params['reference'], params['texte-libre'], oTpe.sVersion, params['code-retour'], params['cvx'], params['vld'], params['brand'], params['status3ds'], params['numauto'], params['motifrefus'], params['originecb'], params['bincb'], params['hpancb'], params['ipclient'], params['originetr'], params['veres'], params['pares']].join('*') + "*";
+
+
+#    if oHmac.isValidHmac?(sChaineMAC, params['MAC'])
+#      case params['code-retour']
+#        when "payetest":
+#          # Payment has been accepted on the test server
+#          # put your code here (email sending / Database update)
+#          @order = Order.find_by_reference(params[:reference].split('A').last)
+#          if @order
+#            @order.pay!
+#            Cart.destroy(@order.reference)
+#          else
+#            status = :bad_request
+#          end
+#        when "paiement":
+#          # Payment has been accepted on the productive server
+#          # put your code here (email sending / Database update)
+#          @order = Order.find_by_reference(params[:reference].split('A').last)
+#          if @order
+#            @order.pay!
+#            Cart.destroy(@order.reference)
+#          else
+#            status = :bad_request
+#          end
+#        else
+#          status = :bad_request
+#        end
+#        sResult = "0"
+#    else
+#      # your code if the HMAC doesn't match
+#      sResult = "1\n" + sChaineMAC
+#    end
+
+#    #-----------------------------------------------------------------------------
+#    # Send receipt to CMCIC server
+#    #-----------------------------------------------------------------------------
+#    render :text => "Pragma: no-cache\nContent-type: text/plain\n\nversion=2\ncdr=" + sResult
+    if @order = Order.find_by_id(params[:order_id])
+      Cart.destroy(@order.reference)
+      @order.update_attribute(:transaction_number,params[:trans_id])
+      if params[:result] == '00'
+        @order.pay!
+      elsif params[:result] == '17'
+        @order.cancel!
+      end
+      render :text => true
+    else
+      render :text => false, :status => 500
+    end
+  end
+
+  
+  def cancel
+    flash[:error] = 'Le paiement à été annulé'
+    redirect_to '/commande-anulee'
+  end
+
+  def success
+    flash[:error] = 'Le paiement à été validé'
+    redirect_to '/commande-validee'
   end
   
   def deliveries
