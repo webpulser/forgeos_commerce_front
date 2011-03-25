100c0df13b484b2a558b3b4325d2fa6c5e7899c5
Jean-Charles Lefrancois <jcl@webpulser.com>
2011-03-25 14:56:37 +0100

    fix pbs with no paymenty options

--- db/migrate/20110322165950_add_payment_methods_to_settings.rb
+++ db/migrate/20110322165950_add_payment_methods_to_settings.rb
@@ -1,6 +1,6 @@
 class AddPaymentMethodsToSettings < ActiveRecord::Migration
   def self.up
-    add_column :settings, :payment_methods, :text
+    add_column :settings, :payment_methods, :text, :default => "--- \n"
   end
 
   def self.down
--- lib/CMCIC_Config.rb
+++ lib/CMCIC_Config.rb
@@ -4,17 +4,18 @@
 # You may for instance put this file in another directory and/or change its name       #
 ########################################################################################
 setting = Setting.first
-cmc_cic_tmp = setting.payment_method_list[:cmc_cic]
-env = cmc_cic_tmp[:test] == 1 ? :development : :production
-cmc_cic = cmc_cic_tmp[env]
+if cmc_cic_tmp = setting.payment_method_list[:cmc_cic]
+  env = cmc_cic_tmp[:test] == 1 ? :development : :production
+  cmc_cic = cmc_cic_tmp[env]
 
-if cmc_cic && cmc_cic_tmp[:active] == 1
-  CMCIC_CLE = cmc_cic[:cle]
-  CMCIC_TPE = cmc_cic[:tpe]
-  CMCIC_VERSION = cmc_cic[:version]
-  CMCIC_SERVEUR = cmc_cic[:serveur]
-  CMCIC_CODESOCIETE = cmc_cic[:code_societe]
-  CMCIC_URLOK = cmc_cic[:url_ok]
-  CMCIC_URLKO = cmc_cic[:url_ko]
-  CMCIC_URL_RETURN = cmc_cic[:url_return]
+  if cmc_cic && cmc_cic_tmp[:active] == 1
+    CMCIC_CLE = cmc_cic[:cle]
+    CMCIC_TPE = cmc_cic[:tpe]
+    CMCIC_VERSION = cmc_cic[:version]
+    CMCIC_SERVEUR = cmc_cic[:serveur]
+    CMCIC_CODESOCIETE = cmc_cic[:code_societe]
+    CMCIC_URLOK = cmc_cic[:url_ok]
+    CMCIC_URLKO = cmc_cic[:url_ko]
+    CMCIC_URL_RETURN = cmc_cic[:url_return]
+  end
 end
