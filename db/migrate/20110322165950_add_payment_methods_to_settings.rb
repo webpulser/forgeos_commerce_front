d9b8968bde12ff332a65e40acc4ebf1f64499397
Jean-Charles Lefrancois <jcl@webpulser.com>
2011-03-25 15:01:02 +0100

    moove mligration to commerce

--- db/migrate/20110322165950_add_payment_methods_to_settings.rb
+++
@@ -1,9 +0,0 @@
-class AddPaymentMethodsToSettings < ActiveRecord::Migration
-  def self.up
-    add_column :settings, :payment_methods, :text, :default => "--- \n"
-  end
-
-  def self.down
-    remove_column :settings, :payment_methods
-  end
-end
