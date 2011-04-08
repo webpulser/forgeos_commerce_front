0c103f943855e217ba826c74b65c2bd95f6889c1
Jean-Charles Lefrancois <jcl@webpulser.com>
2011-04-08 09:40:17 +0200

    working on so_colissimo

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -8,14 +8,71 @@ class OrderController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:call_autoresponse_cyberplus, :paypal_notification, :success, :cancel, :call_autoresponse_cmc_cic, :call_autoresponse_elysnet]
 
   def new
+    setting = Setting.first
+    colissimo = setting.colissimo_method_list
+    p colissimo
     special_offer
     voucher
     @order = Order.from_cart(current_cart)
-    unless @order.valid_for_payment?
+
+    #If so colissimo enabled && params[SIGNATURE]
+    if colissimo && colissimo[:active] == 1 && !params[:SIGNATURE].nil?
+      update_order_with_colissimo
+      unless @order.valid_for_payment?
+        return render :action => 'new'
+      end
+    else
+      p 'la'
+      if @order.valid_for_payment?
+        p 'here'
+        p colissimo[:active]
+        if colissimo[:active] == 1
+          return redirect_to :action => 'so_colissimo'
+        end
+        return render :action => 'new'
+      end
+    end
+  end
+
+  def so_colissimo
+    setting = Setting.first
+    colissimo = setting.colissimo_method_list
+    p colissimo
+    p colissimo[:ulrko]
+    if colissimo && colissimo[:active] == 1
+      if @order = Order.from_cart(current_cart)
+        @_url = "#{colissimo[:url_prod]}?trReturnUrlKo=#{colissimo[:urlko]}"
+        order_infos = @order.to_colissimo_params
+        @colissimo = order_infos
+      else
+        render(:action => 'new')
+      end
+    end
+  end
+
+  def update_order_with_colissimo
+    setting = Setting.first
+
+    s_chaine_mac = [params[:PUDOFOID], params[:CENAME], params[:DYPREPARATIONTIME], params[:DYFORWARDINGCHARGES], params[:TRCLIENTNUMBER], params[:TRORDERNUMBER], params[:ORDERID], setting[:sha] ].join('')
+
+    #pudoFOId + ceName + dyPreparationTime + dyForwardingCharges + trClientNumber+ trOrderNumber+ orderId+cléSHA
+
+    if Digest::SHA1.hexdigest(s_chaine_mac) == params[:SIGNATURE]
+      if @order && @order.reference.to_i == params[:ORDERID].split('m').last.to_i
+        current_cart.update_attributes( :options => params.to_json )
+        @order.update_attributes_from_colissimo(params)
+      else
+        @params = { :order => @order, :order_ref => @order.reference }
+        flash[:error] = "La commande n° #{params[:ORDERID].split('m').last.to_i} est introuvable, identifiant de votre panier est #{@order.reference}"
+        render :action => 'new'
+      end
+    else
+      flash[:error] = 'Une erreur est survenue lors la vérification des données'
+      @params = { :order => @order, :order_ref => @order.reference }
       render :action => 'new'
     end
   end
-  
+
   def create
     special_offer
     voucher
@@ -30,6 +87,7 @@ class OrderController < ApplicationController
     end
     env = setting.payment_method_list[params[:payment_type].to_sym][:test] == 1 ? :development : :production
     @order = Order.from_cart(current_cart)
+
     if params[:validchk]
       @order.payment_type = t(params[:payment_type], :scope => 'payment', :count => 1)
       if @order.valid_for_payment?
@@ -129,7 +187,6 @@ class OrderController < ApplicationController
     end
   end
 
-  
   def cancel
     flash[:error] = 'Le paiement a été annulé'
     redirect_to '/commande-anulee'
