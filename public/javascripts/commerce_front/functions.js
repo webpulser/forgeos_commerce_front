2f7f94d6fdeb8bad1d144adbc14aeb57b2a69f17
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2011-08-05 09:32:21 +0200

    increase jnotify messages delay

--- public/javascripts/commerce_front/functions.js
+++ public/javascripts/commerce_front/functions.js
@@ -1,10 +1,10 @@
 function notifications(){
   $.getJSON('/notifications', function(data) {
     if (data.notice != null)
-      $.jnotify(data.notice);
+      $.jnotify(data.notice, 5000);
     if (data.error != null)
       $.jnotify(data.error,'error',true);
     if (data.warning != null)
-      $.jnotify(data.warning,'warning');
+      $.jnotify(data.warning,'warning', 5000);
   });
 }
